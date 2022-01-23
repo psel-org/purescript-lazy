87e7a07e79716e25c41a20d4bff3da2751059309
kamoii <>
2022-01-24 05:53:04 +0900

    Add .el FFI

---
+++ src/Data/Lazy.el
@@ -0,0 +1,16 @@
+;; -*- lexical-binding: t; -*-
+
+;; JS実装は thunk を undefinedで上書きしている(GCのため？)。
+;; 倣って計算後はthunkをnilにsetqする(引数一つなので部分適用は心配しなくてもいいはず)。
+(defvar Data.Lazy.defer
+  (lambda (thunk)
+    (let ((val nil))
+      (lambda ()
+        (when thunk
+          (setq val (funcall thunk))
+          (setq thunk nil))
+        val))))
+
+(defvar Data.Lazy.force
+  (lambda (l)
+    (funcall l)))
